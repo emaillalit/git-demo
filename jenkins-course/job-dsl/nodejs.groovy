dfd9627c985dde266e75f743ea365d61aad8a560
Lalit <email_lalit@hotmail.com>
2024-02-17 21:06:21 -0800

    Update groovy script to use correct git repo

--- jenkins-course/job-dsl/nodejs.groovy
+++ jenkins-course/job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
-        git('https://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('https://github.com/emaillalit/git-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigEmail('email_lalit@hotmail.com')
         }
     }
     triggers {
